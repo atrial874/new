9b563100743e62078a1340bd2a3386d750d62117
atrial874 <95119887+atrial874@users.noreply.github.com>
2022-05-15 20:19:17 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,17 @@
+class Translation(object):
+
+      
+      START_TEXT = """
+😃 Hi {},
+I am bot by @c4christy
+• Commands
+- /set_cap To Set Caption
+- /set_btn To Set Button
+- /rmv_cap 
+  /rev_btn"""    
+      DYNAMIC_TEXT = """
+🔰 @c4christy"""
+
+
+      MARKDOWN_TEXT = """
+🔰 @c4christy """
